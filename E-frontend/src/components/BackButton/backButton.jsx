ec1f48c572c38a909d7365e4fdb18498cdab7b98
githubcatw <17374742+githubcatw@users.noreply.github.com>
2026-07-19 19:14:16 +0400

    BackButton: remove hardcoded padding

--- E-frontend/src/components/BackButton/backButton.jsx
+++ E-frontend/src/components/BackButton/backButton.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 import ArrowBack from '@mui/icons-material/ArrowBack';
 import Card from '../Card/card'; 
 
-const BackButton = () => {
+const BackButton = (props) => {
     return (
-        <div className="my-5">
+        <div className={props.className}>
             <Card shrink className="w-fit hover:bg-gray-50 p-2">
                 <Link to="/feeds" className="flex justify-between items-center cursor-pointer p-1 rounded transition-colors">
                     <ArrowBack sx={{ color: "gray" }} />
