60ea0add9b0c4d1164f17343fbe5e2db07068a01
githubcatw <17374742+githubcatw@users.noreply.github.com>
2026-07-19 19:22:48 +0400

    Button: reverse padding

--- E-frontend/src/components/Button/button.jsx
+++ E-frontend/src/components/Button/button.jsx
@@ -1,6 +1,6 @@
 const Button = (props) => {
     return (
-        <div className="cursor-pointer px-2 py-4 rounded-xl bg-accent text-white font-semibold" onClick={props.onClick}>
+        <div className="cursor-pointer py-2 px-4 rounded-xl bg-accent text-white font-semibold" onClick={props.onClick}>
             {props.children}
         </div>
     );
